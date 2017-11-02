305bf9ce57793b6e55a9d1110544ff40d665bb4a
pjawneh <pjawneh1@gmail.com>
2017-11-01 20:22:52 -0500

    Initial commit

---
+++ app/src/main/java/io/groupone/pinkpalaceapp/MainActivity.java
@@ -0,0 +1,18 @@
+package io.groupone.pinkpalaceapp;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+}
