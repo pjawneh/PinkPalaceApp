d41253b6140a285157b079ed81c36a48cb134011
pjawneh <pjawneh1@gmail.com>
2017-11-05 01:55:09 -0500

    App Base. TODO: main functionality and remaining activities.

--- app/src/main/java/io/groupone/pinkpalaceapp/MainActivity.java
+++ app/src/main/java/io/groupone/pinkpalaceapp/MainActivity.java
@@ -1,9 +1,16 @@
 package io.groupone.pinkpalaceapp;
 
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +18,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
+        //create viewpager container for tab contents
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(viewPager);
+
+        // Create toolbar tabs
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
     }
+    // Add Fragments to Tabs
+    /*
+    * Setup found via: https://codelabs.developers.google.com/codelabs/material-design-style/index.html?index=..%2F..%2Fio2016#3
+    * */
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new ExhibitsFragment(), "Exhibits");
+        adapter.addFragment(new MapsFragment(), "Maps");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
