c557c20facf94dfcd0a1620f91ea5c1867616c6d
pjawneh <pjawneh1@gmail.com>
2017-11-12 00:33:43 -0600

    App Base Updates. Added: Exhibits/Maps Tab Content TODO: main functionality and remaining activities.

--- app/src/main/java/io/groupone/pinkpalaceapp/ExhibitsFragment.java
+++ app/src/main/java/io/groupone/pinkpalaceapp/ExhibitsFragment.java
@@ -1,10 +1,18 @@
 package io.groupone.pinkpalaceapp;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by pjawneh on 11/5/2017.
@@ -14,6 +22,60 @@ public class ExhibitsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.exhibits_list, null);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return recyclerView;
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView   exhibitImg;
+        private TextView    exhibitTitle;
+
+        private ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.exhibits_list, parent, false));
+            exhibitImg = itemView.findViewById(R.id.list_img);
+            exhibitTitle = itemView.findViewById(R.id.list_title);
+        }
+    }
+
+    /**
+     * Adapter to display recycler view per instructions
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of List in RecyclerView.
+        private static final int LENGTH = 5;
+        private final String[] mExhibits;
+        private final Drawable[] mExhibitImg;
+
+        private ContentAdapter(Context context) {
+            Resources rsc = context.getResources();
+            mExhibits = rsc.getStringArray(R.array.exhibits);
+            TypedArray a = rsc.obtainTypedArray(R.array.exhibit_picture);
+            mExhibitImg = new Drawable[a.length()];
+                for (int i =0; i < mExhibitImg.length; i++){
+                    mExhibitImg[i] = a.getDrawable(i);
+                }
+                a.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.exhibitImg.setImageDrawable(mExhibitImg[position % mExhibitImg.length]);
+            holder.exhibitTitle.setText(mExhibits[position % mExhibits.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
     }
 }
--- app/src/main/java/io/groupone/pinkpalaceapp/MapsFragment.java
+++ app/src/main/java/io/groupone/pinkpalaceapp/MapsFragment.java
@@ -1,10 +1,17 @@
 package io.groupone.pinkpalaceapp;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by pjawneh on 11/5/2017.
@@ -14,6 +21,53 @@ public class MapsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.item_maps, null);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        MapsFragment.ContentAdapter adapter = new MapsFragment.ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return recyclerView;
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mapImg;
+
+        private ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_maps, parent, false));
+            mapImg = itemView.findViewById(R.id.map_img);
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<MapsFragment.ViewHolder> {
+        // Set numbers of List in RecyclerView.
+        private static final int LENGTH = 3;
+        private final Drawable[] mMapImg;
+
+
+        private ContentAdapter(Context context) {
+            Resources rsc = context.getResources();
+            TypedArray a = rsc.obtainTypedArray(R.array.map_picture);
+            mMapImg = new Drawable[a.length()];
+            for (int i =0; i < mMapImg.length; i++){
+                mMapImg[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+
+        @Override
+        public MapsFragment.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MapsFragment.ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(MapsFragment.ViewHolder holder, int position) {
+            holder.mapImg.setImageDrawable(mMapImg[position % mMapImg.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
     }
 }
