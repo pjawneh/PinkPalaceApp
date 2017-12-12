73e68d86dba02e2939e6846fd15dff47d20db75a
Chance <privettec123@gmail.com>
2017-12-12 16:05:28 -0600

    Finished Activity Donate. Needs the next button to be written in Java

---
+++ app/src/main/java/io/groupone/pinkpalaceapp/MemberProfile.java
@@ -0,0 +1,13 @@
+package io.groupone.pinkpalaceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_profile);
+    }
+}
