0c04543087c4457d2a202deb8c5e253e93ab43f4
pjawneh <pjawneh1@gmail.com>
2017-12-12 22:54:34 -0600

    Fixed formatting. Created MemberProfile.java Updated Manifest.

--- app/src/main/java/io/groupone/pinkpalaceapp/MemberProfile.java
+++ app/src/main/java/io/groupone/pinkpalaceapp/MemberProfile.java
@@ -1,7 +1,8 @@
 package io.groupone.pinkpalaceapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MemberProfile extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MemberProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_profile);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
